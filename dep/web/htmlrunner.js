fb7ac2b9ab87768fd50138b976cacce0c3290822
John Resig <jresig@zilla-2.local>
2008-12-04 00:03:23 -0500

    Added some dummy functions for the HTML runner (for testing).

--- dep/web/htmlrunner.js
+++ dep/web/htmlrunner.js
@@ -1,4 +1,4 @@
-var startTest = top.startTest;
-var test = top.test;
-var endTest = top.endTest;
-var prep = top.prep;
+var startTest = top.startTest || function(){};
+var test = top.test || function(name, fn){ fn(); };
+var endTest = top.endTest || function(){};
+var prep = top.prep || function(fn){ fn(); };
